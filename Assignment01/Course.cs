1231a3d70894a6125f096d8e512fbfed4b834acf
JHoffstead-Discover <jonathanhoffstead@discover.com>
2020-08-13 10:19:39 -0500

    Initial

---
+++ Assignment01/Course.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment01
+{
+    public class Course : IComparable, IEnumerable
+    {
+        //Fields
+        private string departmentCode;
+        private uint? courseNumber;
+        private string sectionNumber;
+        private ushort? creditHours;
+        private List<int> enrolled;
+        private ushort? numberOfStudentsEnrolled;
+        private ushort? maxStudentsEnrolled;
+
+        //Properties
+        public string DepartmentCode
+        {
+            get
+            {
+                return departmentCode;
+            }
+            set
+            {
+                if(value.Length > 4)
+                {
+                    throw new ArgumentOutOfRangeException("Error: Department code must be less than 5 characters");
+                }
+                else
+                {
+                    departmentCode = value.ToUpper();
+                }
+            }
+        }
+        public uint? CourseNumber
+        {
+            get
+            {
+                return courseNumber;
+            }
+            set
+            {
+                if(value < 100 || value > 499)
+                {
+                    throw new ArgumentOutOfRangeException("Error: Course number must be between 100 and 499");
+                }
+            }
+        }
+        public string SectionNumber
+        {
+            get
+            {
+                return sectionNumber;
+            }
+            set
+            {
+                if(value.Length != 4)
+                {
+                    throw new ArgumentOutOfRangeException("Error: Section number must be exactly 4 characters");
+                }
+                else
+                {
+                    sectionNumber = value;
+                }
+            }
+        }
+        public ushort? CreditHours
+        {
+            get
+            {
+                return creditHours;
+            }
+            set
+            {
+                if(value < 0 || value > 6)
+                {
+                    throw new ArgumentOutOfRangeException("Error: Credit hours must be between 0 and 6");
+                }
+                else
+                {
+                    creditHours = value;
+                }
+            }
+        }
+        public List<int> Enrolled
+        {
+            get
+            {
+                return enrolled;
+            }
+            set
+            {
+                enrolled = value;
+            }
+        }
+        public ushort? NumberOfStudentsEnrolled
+        {
+            get
+            {
+                return numberOfStudentsEnrolled;
+            }
+            set
+            {
+                numberOfStudentsEnrolled = value;
+            }
+        }
+        public ushort? MaxStudentsEnrolled
+        {
+            get
+            {
+                return maxStudentsEnrolled;
+            }
+            set
+            {
+                maxStudentsEnrolled = value;
+            }
+        }
+
+        //Constructors
+        public Course()
+        {
+            departmentCode = null;
+            courseNumber = null;
+            sectionNumber = null;
+            creditHours = null;
+            enrolled = null;
+            numberOfStudentsEnrolled = null;
+            maxStudentsEnrolled = null;
+        }
+        public Course(string newDepartmentCode, uint newCourseNumber, string newSectionNumber, ushort newCreditHours, List<int> newEnrolled, ushort newNumberofStudentsEnrolled, ushort newMaxStudentsEnrolled)
+        {
+            departmentCode = newDepartmentCode;
+            courseNumber = newCourseNumber;
+            sectionNumber = newSectionNumber;
+            creditHours = newCreditHours;
+            enrolled = newEnrolled;
+            numberOfStudentsEnrolled = newNumberofStudentsEnrolled;
+            maxStudentsEnrolled = newMaxStudentsEnrolled;
+        }
+
+        //Methods
+        public int CompareTo(object obj)
+        {
+            Course otherCourse = obj as Course;
+
+
+        }
+        public Courses(List<int> courseList)
+        {
+            _courses = new Person[pArray.Length];
+
+            for (int i = 0; i < pArray.Length; i++)
+            {
+                _people[i] = pArray[i];
+            }
+
+
+        }
+        public override string ToString()
+        {
+            int? temp = numberOfStudentsEnrolled / maxStudentsEnrolled;
+            return String.Format("{0} {1}-{2} ({3})", departmentCode, courseNumber, sectionNumber, temp);
+        }
+    }
---
+++ Assignment01/Student.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment01
+{
+    public class Student : IComparable
+    {
+        //Fields
+        private readonly uint zid;
+        private string firstName;
+        private string lastName;
+        private string major;
+        public enum academicYear { Freshman, Sophomore, Junior, Senior, PostBacc }
+        private academicYear year;
+        private float? gpa;
+        private ushort? currentCreditHours;
+
+        //Properties
+        public uint Zid
+        {
+            get
+            {
+                return zid;
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string Major
+        {
+            get
+            {
+                return major;
+            }
+            set
+            {
+                major = value;
+            }
+        }
+        public academicYear Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
+        public float? Gpa
+        {
+            get
+            {
+                return gpa;
+            }
+            set
+            {
+                if(value < 0.00 || value > 4.00)
+                {
+                    throw new ArgumentOutOfRangeException("Error: GPA must be between 0.00 and 4.00");
+                }
+                else
+                {
+                    gpa = value;
+                }
+            }
+        }
+        public ushort? CurrentCreditHours
+        {
+            get
+            {
+                return currentCreditHours;
+            }
+            set
+            {
+                if(value < 0 || value > 18)
+                {
+                    throw new ArgumentOutOfRangeException("Error: Credit hours must be between 0 and 18");
+                }
+            }
+        }
+
+        //Constructors
+        public Student()
+        {
+            this.FirstName = null;
+            this.LastName = null;
+            this.Major = null;
+            this.Gpa = null;
+            this.CurrentCreditHours = null;
+
+        }
+        public Student(uint newZid, string newFirstName, string newLastName, string newMajor, academicYear newYear, float newGpa)
+        {
+            if (newZid < 1000000)
+            {
+                throw new ArgumentOutOfRangeException("Error: Zids must be at least 8 characters long");
+            }
+            else
+            {
+                zid = newZid;
+            }
+            FirstName = newFirstName;
+            LastName = newLastName;
+            Major = newMajor;
+            Year = newYear;
+            Gpa = newGpa;
+        }
+
+        //Methods
+        public int CompareTo(object obj)
+        {
+            Student otherStudent = obj as Student;
+            if(this.Zid < otherStudent.Zid)
+            {
+                return -1;
+            }
+            else if(this.Zid == otherStudent.Zid)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        public int Enroll(Course newCourse)
+        {
+
+        }
+        public int Drop(Course newCourse)
+        {
+            if (newCourse.Enrolled.Contains(Convert.ToInt32(this.zid)))
+            {
+                newCourse.Enrolled.Remove(Convert.ToInt32(this.zid));
+                return 0;
+            }
+            else
+            {
+                throw new Exception("Error code 20: Student is not in course");
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format("Z{0} -- {1}, {2} [{3}] ({4}) |{5}|", this.Zid, this.LastName, this.FirstName, this.year, this.Major, this.Gpa);
+        }
+        public string ToString(string type)
+        {
+
+        }
+    }
+}
